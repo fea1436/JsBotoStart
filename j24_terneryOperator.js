f1dec35a40d4ec35db7f11d5c40f3c145a4b8f85
Administrator <Administrator@25609-C>
2022-03-07 12:27:06 +0330

    After ternary & Prompt

---
+++ j24_terneryOperator.js
@@ -0,0 +1,2 @@
+number = 4;
+number > 10 ? console.log('Number is greater than 10') : console.log('Number is less than 10');
---
+++ j25_alert_propmpt_confirm.js
@@ -0,0 +1,5 @@
+// alert("Hello World");
+
+const age = prompt('Please enter your age?');
+console.log(`Age is: ${age}`);
+age > 40 ? console.log('You are old!') : console.log('You are young');
