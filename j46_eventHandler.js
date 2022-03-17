39d536e366bea0bdd37fe4e0d79feb57f1488434
M.E.Aslani <fea1436@gmail.com>
2022-03-18 02:32:43 +0330

    Exercise Episode #46 Implemented

--- j46_eventHandler.js
+++ j46_eventHandler.js
@@ -92,8 +92,68 @@ const text = document.querySelector("input");
 //      console.log("You entered a character");
 // });
 
-// copy input value to a paragraph
-text.addEventListener("keydown", function(event){
-    const paragraphElement = document.querySelector("p");
-    paragraphElement.innerText = event.target.value;
+// KEYDOWN
+// text.addEventListener("keydown", function(event){
+//     console.log(`keydown:  ${event.target.value}`);
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = event.target.value;    
+// });
+
+// KEYUP
+// text.addEventListener("keyup", function(event){
+//     console.log(`keyup:  ${event.target.value}`);
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = event.target.value;    
+// });
+
+// FOCUS ==> element got focus ==> Oposite of BLUR
+// text.addEventListener("focus", function(event){
+//     console.log(`Focus`);
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = event.target.value;    
+// });
+
+// BLUR ==> element losses focus ==> Oposite of FOCUS
+// text.addEventListener("blur", function(event){
+//     console.log(`Blur`);
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = event.target.value;    
+// });
+
+// CUT
+// text.addEventListener("cut", function(event){
+//     console.log(`Cutted value:  ${event.target.value}`);
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = "Cutted value: "+ event.target.value;    
+// });
+
+// PASTE
+// text.addEventListener("paste", function(event){
+//     console.log(`Pateded value:  ${event.target.value}`);
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = "Pasted value: " + event.target.value;    
+// });
+
+
+// KEYPRESS
+// text.addEventListener("keypress", function(event){
+//     const paragraphElement = document.querySelector("p");
+//     paragraphElement.innerText = event.target.value;
+//     console.log(`keypress:  ${event.target.value}`);
+// });
+
+// KEYCODE in KEYDOWN
+const textFull = document.getElementById("input");
+textFull.addEventListener("keydown", function(event){
+    console.log(`keyCode of (${event.key}) is  ${event.keyCode}`);
 });
+
+
+// Different between KEYPRESS & KEYDOWN
+// Control keys (Alt, Control, Escape, Functions, ...) only works with KEYDOWN
+// text.addEventListener("keypress", function(event){
+//     console.log(`key (keypress):  ${event.key}`);
+// });
+// text.addEventListener("keydown", function(event){
+//     console.log(`key (keydown):  ${event.key}`);
+// });
