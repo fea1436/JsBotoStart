6c32c727d0659e942942afe00a826e1c70fa8ee8
M.E.Aslani <fea1436@gmail.com>
2022-04-02 02:44:43 +0430

    Exercise Implemented

--- j47_Calculator.js
+++ j47_Calculator.js
@@ -1,13 +1,40 @@
-let displayBox = document.querySelector('.display');
-let allClear = document.querySelector('.all-clear');
-let clearLast = document.querySelector('.clear-last');
-
 let list = document.querySelectorAll('.show-display');
-list.forEach(item => {
-    item.addEventListener('click', ShowDisplay);
+list.forEach(element => {
+    element.addEventListener('click', showDisplay);
 });
 
-function ShowDisplay(event){
-    const x = event.target.innerText;
-    console.log(x);
+let display = document.querySelector('.display');
+function showDisplay(event) {
+    let content = event.target.innerText;
+    if (display.innerText === "0") {
+        display.innerText = content;
+    }
+    else {
+        display.innerText += content;
+    }
+}
+
+let allClear = document.querySelector('.all-clear');
+allClear.addEventListener('click', clearAll);
+function clearAll()
+{
+    display.innerText = "0";
+}
+
+let clearLast = document.querySelector('.clear-last');
+clearLast.addEventListener('click', lastClear);
+function lastClear(){
+    let content = display.innerText;
+    if (content.length == 1){
+        display.innerText = "0";
+    }else{
+        display.innerText = content.substring(0, content.length - 1);
+    }
+}
+
+let calculate = document.querySelector('.calculate');
+calculate.addEventListener('click', calc);
+function calc(event) {
+    display.innerText = display.innerText.replace('×','*');
+    display.innerText = eval(display.innerText);
 }
