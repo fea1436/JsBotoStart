1f3be971e68293e9bb7909ef2f501ca5ede9b155
fea1436 <fea1436@gmail.com>
2022-04-24 11:29:45 +0430

    Extensions Updates

--- React/Ch02_CreateAnApp/first-app/src/App.js
+++ React/Ch02_CreateAnApp/first-app/src/App.js
@@ -3,17 +3,7 @@ import React, {Component} from "react";
 class App extends Component {
     render() {
         return (
-            <>
-                <div className="container">
-                    <h1>Ehsan Aslani</h1>
-                </div>
-                <p>Hello World</p>
-                
-                {/* <Hi></Hi> */}
-                <Hi />
-                <h2>Another Hi Component After Here</h2>
-                <Salam />
-            </>            
+            <h1>I am a Programmer</h1>   
         )
     }
 }
